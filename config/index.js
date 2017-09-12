8a22065f1c75c0fa3ca451ae8119ed89884b4f4f
jilin <406735191@qq.com>
2017-09-12 14:36:43 +0800

    assetsSubDirectory: '',     assetsPublicPath: '',     為了打包后不生成static     baseUrl '/api'改為'https://www.vue-js.com/api/v1'為了打包后請求不會找不到接口

--- config/index.js
+++ config/index.js
@@ -6,8 +6,8 @@ module.exports = {
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsSubDirectory: '',
+    assetsPublicPath: '',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
@@ -28,13 +28,13 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
-      '/api': {
-        target: 'https://www.vue-js.com/api/v1',
-        changeOrigin: true,
-        pathRewrite:{
-          '^/api': ''
-        }
-      }
+      // '/api': {
+      //   target: 'https://www.vue-js.com/api/v1',
+      //   changeOrigin: true,
+      //   pathRewrite:{
+      //     '^/api': ''
+      //   }
+      // }
     },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
